ea71a03bbe28889f1bc9587b0fe98995bf671bed
Fabiano <git@git.com.br>
2025-05-20 15:33:36 -0300

    add endpoint /replacement

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,12 @@ public class GameListController {
 
 	@GetMapping(value = "/{listId}/games")
 	public List<GameMinDTO> findGames(@PathVariable Long listId) {
+		List<GameMinDTO> result = gameService.findByGameList(listId);
+		return result;
+	}
+
+	@PostMapping(value = "/{listId}/replacement")
+	public List<GameMinDTO> moveGames(@PathVariable Long listId) {
 		List<GameMinDTO> result = gameService.findByGameList(listId);
 		return result;
 	}
