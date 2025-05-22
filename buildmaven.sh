a0266b0394144cde1667420135b7983358fb91b3
Fabiano <git@git.com.br>
2025-05-22 15:19:02 -0300

    Testes final com Postgres

--- buildmaven.sh
+++ buildmaven.sh
@@ -18,6 +18,11 @@
 #apt install maven
 #mvn clean install
 
+#git branch -M main --> forçar usar main ao inves de master
+#git remote set-url origin https://github.com/fabiuniz/dslist-rest.git
+#git branch --> atual
+#git push -f origin main
+#https://github.com/settings/tokens
 
 #mvn spring-boot:run
 
@@ -27,12 +32,21 @@ clear; mvn clean install ; mvn spring-boot:run
 #http://vmlinuxd:8080/swagger-ui/index.html
 #http://vmlinuxd:8080
 #http://vmlinuxd:5050/ pgamin
-#
+#PGADMIN_DEFAULT_EMAIL: me@example.com
+#Servers->registrar->servidor->general->name=Postgress-local-docker
+#Servers->registrar->servidor->Connection->hostname/adress=pg-docker
+#	->prot=5433
+#	->maintenance database=mydatabase
+#	->user=postgres
+#	->senah=1234567
+#	database->createdabase=dslist
+#	dslist->schemas->public->tables-Querytool= Colar script
 #apt-get install ufw
 #ufw status
 #ufw enable
 #ufw status verbose
 #ufw allow 5050/tcp
+#ufw allow 5432/tcp
 #ufw allow 5433/tcp
 #ufw status verbose
 #ufw reload
@@ -50,3 +64,4 @@ clear; mvn clean install ; mvn spring-boot:run
 #
 #cd /usr/lib/jvm/
 #tar -xvzf /home/userlnx/docker/script_docker/relay/zulu21.42.19-ca-fx-jdk21.0.7-linux_x64.tar.gz
+#
