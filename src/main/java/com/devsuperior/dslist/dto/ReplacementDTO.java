bd5f32ad2ac7507ec35ec790428f15ef26acc33b
Fabiano <git@git.com.br>
2025-05-22 15:17:24 -0300

    Endpoint de movimentação da lista url/replacement Controller → Service(move()) → Repository → Service → GameMinProjection → Controller

--- src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -4,14 +4,6 @@ public class ReplacementDTO {
     private Integer sourceIndex;
     private Integer destinationIndex;
 
-    public ReplacementDTO() {
-    }
-
-    public ReplacementDTO(Integer sourceIndex, Integer destinationIndex) {
-        this.sourceIndex = sourceIndex;
-        this.destinationIndex = destinationIndex;
-    }
-
     public Integer getSourceIndex() {
         return sourceIndex;
     }
